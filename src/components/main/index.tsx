ca320384a0e5dde3ed11c57ad4b2884899cd0a2b
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2021-07-25 17:22:07 +0900

    feat: test css

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import './index.css';
 
 function Main() {
-  return <div>hello;</div>;
+  return (
+    <div>
+      <div className="title">Meet In</div>
+      <div className="content">
+        <div>
+          <button type="button">로그인</button>
+        </div>
+        <div>
+          <button type="button">로그인 없이 시작</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
