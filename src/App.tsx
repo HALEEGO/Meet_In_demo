4c986fe6c89e38808a36a3a239d18f2dd82fa0d5
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2021-07-28 22:38:08 +0900

    feat: navigation routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './components/main';
+import Login from './components/login';
+import SignUp from './components/signUp';
+import Home from './components/home';
+import MakeRoom from './components/makeRoom';
+import Room from './components/room';
 
-function App() {
-  return <Main />;
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact render={() => <Main />} />
+      <Route path="/login" exact component={Login} />
+      <Route path="/signUp" exact component={SignUp} />
+      <Route path="/home" exact component={Home} />
+      <Route path="/makeRoom" exact component={MakeRoom} />
+      <Route path="/room" exact component={Room} />
+    </Switch>
+  </Router>
+);
+
+// eslint-disable-next-line no-lone-blocks
+{
+  /* <Link to={{ pathname: '/talk', state: { hi: name, number: roomID } }}> // 링크 투 예제 -> chat_app main.tsx 참고해주셈.
+            <button
+              type="button"
+              onClick={() => {
+                setRoomID('');
+              }}
+            >
+              send
+            </button>
+          </Link> */
 }
 
 export default App;
