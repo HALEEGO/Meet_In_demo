36c6efa9f600648bfe19e6d4911e45fe9cc02d87
sfseed3y <78024945+sfseed3y@users.noreply.github.com>
2021-07-28 22:37:09 +0900

    feat: background and images
    even some navbar

--- src/components/main/navbar.tsx
+++ src/components/main/navbar.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import styles from './navbar.module.css';
+import meetigtmln from './meetin.png';
 
 function Navbar() {
   return (
     <div className={styles.navbar}>
-      <div className={styles.logo}>Logo</div>
+      <div className={styles.logo}>
+        <img src={meetigtmln} alt="dd" />
+      </div>
       <div className={styles.button}>menu1</div>
       <div className={styles.button}>menu2</div>
-      <div className={styles.button}>Login</div>
+      <button type="button">Login</button>
     </div>
   );
 }
